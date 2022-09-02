947a960b41a9e4dee58da6618893786673040617
Mitran Matei <m.mitran@student.fontys.nl>
2022-09-02 13:45:14 +0200

    Weather API setup

---
+++ pages/api.service.js
@@ -0,0 +1,11 @@
+import axios from 'axios';
+
+
+
+class ApiService {
+
+    sendRequest(query) {
+         return axios.post("http://api.weatherstack.com/current?access_key=cfd3ad17303f6603902b474daa1f3c6f&query=" + query, "")
+    }
+}
+export default new ApiService();
