e20740b8eee3769f16739a9e2a3363d7ab17f745
Mitran Matei <m.mitran@student.fontys.nl>
2022-09-12 08:12:03 +0200

    Refactored Cypress tests

--- cypress/e2e/SendButtonTest.cy.js
+++ cypress/e2e/SendButtonTest.cy.js
@@ -1,40 +1,39 @@
-
-describe('Test Weather API', () => {
+describe("Test Weather API", () => {
   it('finds the content "Send"', () => {
-    cy.visit('http://localhost:3000')
-    
+    cy.visit("http://localhost:3000");
 
-    cy.get('[data-cy="input_Textfield"]').type("Bucharest")
+    cy.getBySel("input_Textfield").type("Bucharest");
 
-    cy.get('[data-cy="send_Button"]').click()
+    cy.getBySel("send_Button").click();
 
-    cy.get('[data-cy="card_Label"]').should('be.visible')
-  })
-}) ,
-
-describe('Test Reset Button', () => {
-  it('finds the content "Send"', () => {
-    cy.visit('http://localhost:3000')
-    
-    cy.get('[data-cy="input_Textfield"]').type("Bucharest")
-
-    cy.get('[data-cy="send_Button"]').click()
-
-    cy.get('[data-cy="card_Label"]').should('be.visible')
-
-    cy.get('[data-cy="reset_Button"]').click()
-
-    cy.get('[data-cy="card_Label"]').should("not.exist")
-  })
+    cy.getBySel("card_Label").should("be.visible");
+  });
 }),
+  // ADD Cypress.Add
+  describe("Test Reset Button", () => {
+    it('finds the content "Send"', () => {
+      cy.visit("http://localhost:3000");
 
-describe("Test POST Request", () => {
-  it("Tests the post http request", () => {
+      cy.getBySel("input_Textfield").type("Bucharest");
 
-    cy.intercept('http://api.weatherstack.com/current?access_key=cfd3ad17303f6603902b474daa1f3c6f&query=Bucharest', (req) => {
-      req.continue((res) => {
-        expect(res.body).to.include("temperature")
-      })
-    })
-  })
-})
+      cy.getBySel("send_Button").click();
+
+      cy.getBySel("card_Label").should("be.visible");
+
+      cy.getBySel("reset_Button").click();
+
+      cy.getBySel("card_Label").should("not.exist");
+    });
+  }),
+  describe("Test POST Request", () => {
+    it("Tests the post http request", () => {
+      cy.intercept(
+        "http://api.weatherstack.com/current?access_key=cfd3ad17303f6603902b474daa1f3c6f&query=Bucharest",
+        (req) => {
+          req.continue((res) => {
+            expect(res.body).to.include("temperature");
+          });
+        }
+      );
+    });
+  });
